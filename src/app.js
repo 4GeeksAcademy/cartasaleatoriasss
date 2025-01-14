577906a66da1eda3113bfe9c0b60c251ffc07902
francoaguilar622 <123565045+francoaguilar622@users.noreply.github.com>
2025-01-14 18:23:10 +0000

    cartas terminadas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let simbolo = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  let randomsimboloNumber = Math.floor(Math.random() * simbolo.length);
+  let finalsimbolo = simbolo[randomsimboloNumber];
+
+  document.getElementById("cardContent").innerHTML =
+    cardNumber[randomCardNumber];
+  document.getElementById("theCard").className = "";
+  document.getElementById("theCard").classList.add("card");
+  document
+    .getElementById("theCard")
+    .classList.add(getsimboloeClass(finalsimbolo));
 };
+
+function getsimboloeClass(simbolo) {
+  switch (simbolo) {
+    case "Diamonds":
+      return "simbolo-diamonds";
+    case "Spades":
+      return "simbolo-spades";
+    case "Hearts":
+      return "simbolo-hearts";
+    case "Clubs":
+      return "simbolo-clubs";
+  }
+}
